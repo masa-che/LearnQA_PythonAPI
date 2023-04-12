2fddde43be57979782c381591dee98c55fa81cc6
masa-che <https://github.com/masa-che/LearnQA_PythonAPI.git>
2023-04-12 13:43:33 +0300

    siple api requests

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("Hello from Maxim")
+import requests
+
+payload = {"name": "player_one"}                                                     # параметр dict{}
+response = requests.get('https://playground.learnqa.ru/api/hello', params=payload)   # get запрос к url адресу
+print(response.text)                                                                 # print в формате json
 
