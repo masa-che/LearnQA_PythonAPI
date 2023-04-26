96edfba6bcf2b79e4c0a8775d3e3ab9e2b061ade
masa-che <https://github.com/masa-che/LearnQA_PythonAPI.git>
2023-04-26 20:41:52 +0300

    add hw_2 last task and new ex in module number 3

--- Simple_Requests_to_API_2/HomeWork.py
+++ Simple_Requests_to_API_2/HomeWork.py
@@ -9,7 +9,7 @@ import time
 # json формат, обернули в переменную, перед преобразованием
 string_as_json_format = '{"messages":[{"message":"This is the first message","timestamp":"2021-06-04 16:40:53"},' \
                         '{"message":"And this is a second message","timestamp":"2021-06-04 16:41:01"}]}'
-obj = json.loads(string_as_json_format)                # методом loads преобр нашу строку из json в объект python {dict}
+obj = json.loads(string_as_json_format)                # метод loads преобразует строку из json в объект python {dict}
 print(obj['messages'][1])                              # в словаре (obj) по ключу, принтуем 2-е сообщение
 
 # Задача 6 Длинный редирект
@@ -107,3 +107,43 @@ if obj['status'] == "Job is ready" and obj['result'] == "42":
 else:
     print("not expected result")
 
+# Задача 9 "Подбор пароля"
+# Данные из статьи в википедии "Top 25 most common passwords by year according to SplashData"
+password_str = " password 123456 123456789 12345678 12345 qwerty abc123 qwerty football" \
+               " 1234567 monkey 111111 letmein 1234 1234567890" \
+               " 1234567	12345letmein	dragon	1234567	baseball	1234	1234567	1234567	sunshine" \
+               " iloveyou trustno1	111111	iloveyou	dragon	1234567	princess	football	qwerty" \
+               " 111111 dragon	baseball	adobe123[a]	football	baseball	1234	iloveyou	iloveyou" \
+               " 123123" "baseball	iloveyou	123123	1234567	welcome	login	admin	princess" \
+               " abc123 111111	trustno1	admin	monkey	1234567890	welcome	welcome	admin	" \
+               " qwerty123 iloveyou	1234567	1234567890	letmein	abc123	solo	monkey	welcome	1q2w3e4r master" \
+               " sunshine	letmein	abc123	111111	abc123	login	666666	admin sunshine	master	photoshop[a]" \
+               " 111111	1qaz2wsx	admin	abc123	abc123	qwertyuiop ashley	123123	1234	mustang	dragon" \
+               " 121212	starwars	football	654321 bailey	welcome	monkey	access	master" \
+               " flower	123123	123123	555555 passw0rd	shadow	shadow	shadow	monkey	passw0rd" \
+               " dragon	monkey	lovely shadow	ashley	sunshine	master	letmein	dragon	passw0rd" \
+               " 654321	7777777 123123	football	12345	michael	login	sunshine	master	!@#$%^&*" \
+               " welcome 654321	jesus	password1	superman	princess	master	hello	charlie" \
+               " 888888 superman	michael	princess	696969	qwertyuiop	hottie	freedom	aa123456" \
+               " princess qazwsx	ninja	azerty	123123	solo	loveme	whatever	donald	dragonmichael" \
+               " mustang	trustno1 batman	passw0rd	zaq1zaq1	qazwsx	password1	password1 Football" \
+               " password1	000000	trustno1	starwars	password1	trustno1	qwerty123	123qwe"
+print(password_str.split())                                # split создаёт список и каждое слово в str разделяет запятой
+print(len(password_str.split()))                           # длина списка 172 - элемента
+password_list = password_str.split()
+top_password_list = []
+[top_password_list.append(x) for x in password_list if x not in top_password_list]  # редактирование списка, для "устранения" повторяющихся эл-ов
+print(top_password_list)                                   # при помощи нового списка убрали повторения
+print(len(top_password_list))                              # длина нового списка 72 - элемента
+
+URL = "https://playground.learnqa.ru/ajax/api/get_secret_password_homework"
+for password in top_password_list:
+    response = requests.post(URL, data={"login": "super_admin", "password": password})
+    # print(response.cookies)
+    cookie_value = response.cookies.get('auth_cookie')     # забираем значение cookie из dict по ключу 'auth_cookie'
+    cookie = {'auth_cookie': cookie_value}
+    check_response = requests.post('https://playground.learnqa.ru/ajax/api/check_auth_cookie', cookies=cookie)
+    check_auth = check_response.text
+    # print(check_auth)                               # проверка текстового ответа перебора 72 эл-в "You are authorized"
+    if check_auth == "You are authorized":
+        print(f"{check_auth} password = {password}")
---
+++ api_pytest_3/3_1simple_test.py
@@ -0,0 +1,12 @@
+class TestExample:
+    def test_check_math(self):
+        a = 4
+        b = 4
+        expected_sum = 8
+        assert a+b == expected_sum, "Sum of variable isn't equal"
+
+    def test_check_error(self):
+        a = 4
+        b = 3
+        expected_sum = 8
+        assert a+b == expected_sum, "Sum of variable isn't equal"
