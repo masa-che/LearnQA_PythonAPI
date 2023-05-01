e58b043c1791a2ca46638da2c650a87528f1288a
masa-che <https://github.com/masa-che/LearnQA_PythonAPI.git>
2023-05-01 12:39:34 +0300

    add new ex 3_5

--- api_pytest_3/3_4_5positive_negative_auth_test.py
+++ api_pytest_3/3_4_5positive_negative_auth_test.py
@@ -42,14 +42,14 @@ class TestPositiveAuth:
 
 
 # Задание 3_4_5 Негативные тесты на авторизацию пользователя
-    exclude_params = {
-        'no_cookie',
-        'no_token'
+    exclude_params = {                                          # параметры для негативного теста с куки и токеном
+        'no_cookie',                                            # в тесте будет либо куки, либо токен
+        'no_token'                                              # без которых user_id пользователя должен == 0
     }
 
-    @pytest.mark.parametrize('condition', exclude_params)
-    def test_negative_auth_user(self, condition):
-        data = {
+    @pytest.mark.parametrize('condition', exclude_params)       # подключаем в виде параметра exclude_params и
+    def test_negative_auth_user(self, condition):               # в тесте будем вызывать пременную condition со значениями словаря exclude_params
+        data = {                                                # далее идёт часть кода из "3_4", до второго запроса
             'email': 'vinkotov@example.com',
             'password': '1234'
         }
@@ -62,5 +62,15 @@ class TestPositiveAuth:
         auth_sid = response.cookies.get('auth_sid')
         token = response.headers.get('x-csrf-token')
 
+# далее идёт код со значениями из словаря exclude_params, где мы передаём в запрос только параметр с куки или токеном
+        if condition == 'no_cookie':
+            response0 = requests.get("https://playground.learnqa.ru/api/user/auth", headers={'x-csrf-token': token})
+        else:
+            response0 = requests.get("https://playground.learnqa.ru/api/user/auth", cookies={'auth_sid': auth_sid})
 
+        assert 'user_id' in response.json(), "There no user id in the response"
+
+        user_id_from_check_method = response0.json()['user_id']
+
+        assert user_id_from_check_method == 0, f"User is authorized with condition {condition}"
 
