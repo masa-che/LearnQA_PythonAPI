c1010d0ae524a7373460a5571e2116a71127227d
masa-che <https://github.com/masa-che/LearnQA_PythonAPI.git>
2023-05-27 20:28:21 +0300

    add 13 task in hw

--- api_pytest_3/test_hw.py
+++ api_pytest_3/test_hw.py
@@ -1,7 +1,8 @@
 import requests
-
+import pytest
 
 # Задача 10. Тест на короткую фразу не больше 15ти символов.
+
 
 class Test:
     def test_input_phrase(self):
@@ -53,4 +54,48 @@ class TestRequestMethodHeader:
         assert check_header == 'Some secret value', "Wrong value header 'x-secret-homework-header'"
 
 
+# Задача 13. User_Agent
 
+class TestUserAgent:
+    user_agent_data = [
+        {
+            "user_agent": "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+            "platform": "Mobile",
+            "browser": "No",
+            "device": "Android"},
+        {
+            "user_agent": "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+            "platform": "Mobile",
+            "browser": "Chrome",
+            "device": "iOS"},
+        {
+            "user_agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "platform": "Googlebot",
+            "browser": "Unknown",
+            "device": "Unknown"},
+        {
+            "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+            "platform": "Web",
+            "browser": "Chrome",
+            "device": "No"},
+        {
+            "user_agent": "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+            "platform": "Mobile",
+            "browser": "No",
+            "device": "iPhone"}
+    ]
+
+    @pytest.mark.parametrize("user_agent_data", user_agent_data)
+    def test_user_agent(self, user_agent_data):
+        user_agent = user_agent_data["user_agent"]
+        response = requests.get("https://playground.learnqa.ru/ajax/api/user_agent_check",
+                                headers={"User-Agent": user_agent})
+
+        actual_result = response.json()
+        print(actual_result)
+        assert actual_result['platform'] in user_agent_data['platform'],\
+            f"The '{actual_result['platform']}' does not match the expected result"
+        assert actual_result['browser'] in user_agent_data['browser'],\
+            f"The '{actual_result['browser']}' does not match the expected result"
+        assert actual_result['device'] in user_agent_data['device'],\
+            f"The '{actual_result['device']}' does not match the expected result"
