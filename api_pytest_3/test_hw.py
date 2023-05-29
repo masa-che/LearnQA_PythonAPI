0ce62fd4eb5aba15fddeafba0e2b7334135f6b5b
masa-che <https://github.com/masa-che/LearnQA_PythonAPI.git>
2023-05-29 06:33:39 +0300

    add new contetnt in hw 13 task

--- api_pytest_3/test_hw.py
+++ api_pytest_3/test_hw.py
@@ -57,7 +57,7 @@ class TestRequestMethodHeader:
 # Задача 13. User_Agent
 
 class TestUserAgent:
-    user_agent_data = [
+    user_agent_information = [
         {
             "user_agent": "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
             "platform": "Mobile",
@@ -85,7 +85,7 @@ class TestUserAgent:
             "device": "iPhone"}
     ]
 
-    @pytest.mark.parametrize("user_agent_data", user_agent_data)
+    @pytest.mark.parametrize("user_agent_data", user_agent_information)
     def test_user_agent(self, user_agent_data):
         user_agent = user_agent_data["user_agent"]
         response = requests.get("https://playground.learnqa.ru/ajax/api/user_agent_check",
